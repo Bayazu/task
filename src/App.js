6fc81824e3616b1d375619d13e37e30b86c7a531
Bayauz <sasha.titov.00@list.ru>
2021-11-12 15:34:32 +0600

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+          <div className='App'>
+                  <Navbar />
+                  <div className='app-wrapper-content'>
+                      {/*<Route path='/admission' component={Admission} />*/}
+                      {/*<Route path='/directions' component={Directions} />*/}
+                      {/*<Route path='/documents' component={Documents} />*/}
+                      {/*<Route path='/isu' component={Isu} />*/}
+                      {/*<Route path='/adm' component={Adm} />*/}
+                  </div>
+          </div>
+
+      </BrowserRouter>
+
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,13 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import {MenuItems} from "./MenuItems";
 import './Navbar.css'
-import {Button} from "../../../../../hakaton/frontend/src/components/Button/Button";
-import Modal from "../../../../../hakaton/frontend/src/components/Modal/Modal";
-import emailjs from 'emailjs-com';
+//import {Button} from "../../../../../hakaton/frontend/src/components/Button/Button";
+import Modal from "../Modal/Modal";
 
 const Navbar = () => {
 
     const [clicked, setClicked] = useState(false)
     const [modalActive, setModalActive] = useState(false)
-    //const [text, setText] = useState('')
-
-    const form = useRef();
-
-    function sendEmail(e) {
-        e.preventDefault();
-
-        emailjs.sendForm('service_xnn74r9', 'template_d5ovedm', form.current, 'user_zoN8QqlaFX1DRxQLMYQv8')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.target.reset()
-    }
-
-
-
 
     return (
         <nav className='NavbarItems'>
@@ -46,30 +27,31 @@ const Navbar = () => {
                     )
                 })}
             </ul>
-            <Button onClick={() => setModalActive(true)}>Боитесь забыть о поступлении?</Button>
+            {/*<Button onClick={() => setModalActive(true)}>Боитесь забыть о поступлении?</Button>*/}
+
             <Modal active={modalActive} setActive={setModalActive}>
+
                 <div>
-                    Если вы боитесь что-то забыть или пропустить, то введите свой email и вам будут приходить
-                    уведомления о всех важных событиях
+                    тут будет регистрация
                 </div>
+                {/*<div>*/}
+                {/*    Если вы боитесь что-то забыть или пропустить, то введите свой email и вам будут приходить*/}
+                {/*    уведомления о всех важных событиях*/}
+                {/*</div>*/}
 
-                <form ref={form} onSubmit={sendEmail}>
-                    <label>ФИО</label>
-                    <input type="text" name="name" className="type-1" />
-                    <label>Email</label>
-                    <input type="email" name="user_email" className="type-1" />
-                    <input type="submit" value="Send"  className="type-1" onClick={()=>{
-                        setModalActive(false)
-                    }}/>
-                    <label>Я соглашаюсь с обработкой персональных данных
-                        <input type="checkbox"/>
-                        <span></span>
-                    </label>
-                </form>
-
-
-
-
+                {/*<form ref={form} onSubmit={sendEmail}>*/}
+                {/*    <label>ФИО</label>*/}
+                {/*    <input type="text" name="name" className="type-1"/>*/}
+                {/*    <label>Email</label>*/}
+                {/*    <input type="email" name="user_email" className="type-1"/>*/}
+                {/*    <input type="submit" value="Send" className="type-1" onClick={() => {*/}
+                {/*        setModalActive(false)*/}
+                {/*    }}/>*/}
+                {/*    <label>Я соглашаюсь с обработкой персональных данных*/}
+                {/*        <input type="checkbox"/>*/}
+                {/*        <span></span>*/}
+                {/*    </label>*/}
+                {/*</form>*/}
             </Modal>
 
         </nav>
